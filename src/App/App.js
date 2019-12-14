c31f11f2d10bf95f90374f32e79135f78feef150
Randy Tate <randytatejr@gmail.com>
2019-12-13 21:03:15 -0600

    completed second part of setup

--- src/App/App.js
+++ src/App/App.js
@@ -2,27 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button className="btn btn-danger">Randy's Button</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+          <button className="btn btn-danger">Shark Attack</button>
+      </div>
+    );
+  }
 }
 
 export default App;
