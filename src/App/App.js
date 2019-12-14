c26554a1cc14cec2b93d5d876b3002119b472f3c
Randy Tate <randytatejr@gmail.com>
2019-12-14 09:14:18 -0600

    WIP

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './App.scss';
+
 import SharkTank from '../components/SharkTank/sharkTank';
+import studentData from '../helpers/data/studentData';
 
 class App extends React.Component {
+  state= {
+    students: [],
+  }
+
+  componentDidMount() {
+    const students = studentData.getStudents();
+    this.setState({ students });
+  }
 
   render() {
     return (
       <div className="App">
           <button className="btn btn-danger">Shark Attack</button>
-          <SharkTank />
+          <SharkTank students={this.state.students} />
       </div>
     );
   }
--- src/components/SharkTank/sharkTank.js
+++ src/components/SharkTank/sharkTank.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import LiveStudent from '../LiveStudent/liveStudent';
+
+import studentShape from '../../helpers/propz/studentShape';
 
 class SharkTank extends React.Component {
+  static propTypes = {
+    students: PropTypes.arrayOf(studentShape.studentShape),
+  }
+
   render() {
+    const myStudents = this.props.students;
+
+    const studentCards = myStudents.map((student) => <LiveStudent key={student.id} student={student} />);
     return (
       <div className="sharkTank row">
-
+        {studentCards}
       </div>
     );
   }
